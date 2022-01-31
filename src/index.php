f27c2e580f0ea00e040d79909ad302ec20ba913b
dalila <dalila.yazit@gmail.com>
2022-01-31 09:49:39 +0100

    changement date

--- src/index.php
+++ src/index.php
@@ -1,6 +1,5 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 10 janvier 2023 </p>';
+echo '<p>update : 31 janvier 2023 </p>';
 ?>
-
